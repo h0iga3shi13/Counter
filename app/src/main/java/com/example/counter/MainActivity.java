711a8430a993040d3d9345ed4be1a7172c827656
UserName <easton3.h@gmail.com>
2020-08-17 21:59:43 +0900

    minus1Button追加 ボタンを押すと１ずつ数字を減らす処理

--- app/src/main/java/com/example/counter/MainActivity.java
+++ app/src/main/java/com/example/counter/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView countText;
     private Button plus1Button;
+    private Button minus1Button;
     int number;
 
     @Override
@@ -20,15 +21,26 @@ public class MainActivity extends AppCompatActivity {
 
         countText = (TextView)findViewById(R.id.text);
         plus1Button = (Button)findViewById(R.id.plus1);
+        minus1Button = (Button)findViewById(R.id.minus1);
         // 変数numberに0を代入する
         number = 0;
 
+        // plusButtonの処理
         countText.setText(number + "");
         plus1Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 // ボタンが押されるごとに1ずつ数字が増えていく処理
                 number++;
+                countText.setText(number + "");
+            }
+        });
+
+        //minusButtonの処理
+        minus1Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                number--;
                 countText.setText(number + "");
             }
         });
